fe19129a6f75e5ff9e71b5c03bc824e7c90da723
visualstudio <visualstudio@122-PC30>
2019-01-18 21:44:34 -0500

    commit2

--- c0730406/Program.cs
+++ c0730406/Program.cs
@@ -10,25 +10,52 @@ namespace c0730406
     {
         static void Main(string[] args)
         {
-            StudentInformation _Mary = new StudentInformation("Mary" , 3.8 , 20);
-            StudentInformation _john = new StudentInformation("john" , 3.8 , 20);
-            StudentInformation _Mark = new StudentInformation("Mark", 3.8 , 20);
-            StudentInformation _Steve= new StudentInformation("Steve", 3.8 , 20);
-            StudentInformation _Cindy = new StudentInformation("Cindy" , 3.8 , 20);
-            Console.ReadLine();
-          }
-    }
-    class StudentInformation
-    {
-        public string name;
+            {
+                StudentInformation Mary = new StudentInformation("Mary", 3.8, 20);
+                StudentInformation John = new StudentInformation("John", 1.8, 2);
+                StudentInformation Mark = new StudentInformation("Mark", 2.5, 3);
+                StudentInformation Steve = new StudentInformation("Steve", 3.3, 18);
+                StudentInformation Cindy = new StudentInformation("Cindy", 2.3, 12);
 
-        public double GPA;
-        public int attendence;
-        public StudentInformation(string aName, double aGPA, int dAttendence)
+
+                var StudentList = new List<StudentInformation>();
+
+                StudentList.Add(Mary);
+                StudentList.Add(John);
+                StudentList.Add(Mark);
+                StudentList.Add(Steve);
+                StudentList.Add(Cindy);
+
+                foreach (var Student in StudentList)
+                {
+                    Console.WriteLine(Student.Name);
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+        class StudentInformation
         {
-            name = aName;
-            GPA = aGPA;
-            attendence = dAttendence;
+            public string Name;
+
+            public double GPA;
+            public int Attn;
+
+            public StudentInformation(string aName, double aGPA, int aAttn)
+            {
+                Name = aName;
+                GPA = aGPA;
+                Attn = aAttn;
+            }
+
+        }
+
+        class StudentList
+        {
+            public void CICS()
+            {
+            }
         }
     }
 }
